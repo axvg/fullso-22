fbae349e539d2e948950503489b6f8a0694a16f7
axvg <a52vega@gmail.com>
2022-05-15 23:36:00 -0500

    ex. 1.7.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,22 +1,24 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <button onClick={() => setGood(good + 1)}>good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>average {(good - bad) / (good + neutral + bad)}</p>
+            <p>positive { good * 100 / (good + neutral + bad)}%</p>
+        </div>
+    );
+};
 
-export default App
+export default App;
