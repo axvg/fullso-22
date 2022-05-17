14512306cb5ec5e2a7f3220debc864aa13cddbed
axvg <a52vega@gmail.com>
2022-05-16 22:41:15 -0500

    ex. 1.11.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,34 +7,44 @@ const Button = ({ handleClick, text }) => {
 const StatisticLine = ({ text, value }) => {
     if (text === 'positive') {
         return (
-            <p>
-                {text} {value} %
-            </p>
+            <tr>
+                <td>{text}</td>
+                <td>{value} %</td>
+            </tr>
         );
     }
     return (
-        <p>
-            {text} {value}
-        </p>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     );
 };
 
 const Statistics = ({ good, neutral, bad }) => {
     if (good + neutral + bad !== 0) {
         return (
-            <div>
-                <StatisticLine text='good' value={good} />
-                <StatisticLine text='neutral' value={neutral} />
-                <StatisticLine text='bad' value={bad} />
-                <StatisticLine
-                    text='average'
-                    value={(good - bad) / (good + neutral + bad)}
-                />
-                <StatisticLine
-                    text='positive'
-                    value={(good * 100) / (good + neutral + bad)}
-                />
-            </div>
+            <table>
+                <tbody>
+                    <StatisticLine text='good' value={good} />
+
+                    <StatisticLine text='neutral' value={neutral} />
+
+                    <StatisticLine text='bad' value={bad} />
+
+                    <StatisticLine text='all' value={good + neutral + bad} />
+
+                    <StatisticLine
+                        text='average'
+                        value={(good - bad) / (good + neutral + bad)}
+                    />
+
+                    <StatisticLine
+                        text='positive'
+                        value={(good * 100) / (good + neutral + bad)}
+                    />
+                </tbody>
+            </table>
         );
     } else {
         return (
@@ -54,7 +64,10 @@ const App = () => {
         <div>
             <h1>give feedback</h1>
             <Button handleClick={() => setGood(good + 1)} text='good' />
-            <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+            <Button
+                handleClick={() => setNeutral(neutral + 1)}
+                text='neutral'
+            />
             <Button handleClick={() => setBad(bad + 1)} text='bad' />
 
             <h1>statistics</h1>
