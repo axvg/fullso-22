9855ed6cc8b9d72a7bc56641bf48b31554a15bbc
axvg <a52vega@gmail.com>
2022-06-13 23:50:52 -0500

    e2-2--2.5

--- part2/courseinfo-2/src/components/Course.js
+++ part2/courseinfo-2/src/components/Course.js
@@ -2,10 +2,13 @@ import Content from './Content';
 import Header from './Header';
 
 const Course = ({ course }) => {
+    const total = course.parts.reduce((s, p) => s + p.exercises, 0)
+
     return (
         <div>
             <Header name={course.name} />
             <Content course={course} />
+            <strong>Total of {total} exercises</strong>
         </div>
     );
 };
